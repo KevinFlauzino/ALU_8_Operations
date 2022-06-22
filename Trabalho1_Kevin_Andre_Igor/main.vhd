a3daca130aeda76cb836cd7395b89f2370b7c719
Kevin Flauzino do Nascimento <kevin.flauzino@hotmail.com>
2022-06-22 18:43:58 -0300

    Começando o corpo da ULA
    Mexi mais na "main", começando o corpo da ULA, tentar arrumar o erro da linha 108 que o debug mostra.

--- Trabalho1_Kevin_Andre_Igor/main.vhd
+++ Trabalho1_Kevin_Andre_Igor/main.vhd
@@ -30,9 +30,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity Main is
-	PORT (N1, N2, ESC: in STD_LOGIC_VECTOR(3 downto 0);
-			Reset, Confirma: in STD_LOGIC;
-			Resultado: out STD_LOGIC_VECTOR(7 downto 0)
+	PORT (n1, n2, esc: in STD_LOGIC_VECTOR(3 downto 0);
+			reset, confirma: in STD_LOGIC;
+			resultado: out STD_LOGIC_VECTOR(7 downto 0)
 			);
 end Main;
 
@@ -59,20 +59,53 @@ architecture Behavioral of Main is
 				D: out STD_LOGIC_VECTOR (3 downto 0);
 				Cin: in STD_LOGIC);
 	END COMPONENT;
-
+	
+	SIGNAL cout: STD_LOGIC;
+	
 begin
 
-	PROCESS ULA(Reset, Confirma)
-	
-		--Declarando variáveis
-		VARIABLE Confirma_var : STD_LOGIC;
+	PROCESS(reset, confirma)	
+		--Declarando variáveis		
 		
 		--Início da ULA
 		BEGIN
-		if Confirma = "1" then
-			N1 <= --switches			
+			if confirma = "1" then
+				n1 <= "0";  --switches
+			end if;
+				
+			wait until confirma'event;
+				if confirma = "1" then
+				n2 <= "0";--switches
+				end if;
 			
-
+			wait until confirma'event;
+				if confirma = "1" then
+				esc <= "0";--switches
+				-------------------------------------------Op.Lógicas
+					If esc = "0000" then 			--AND
+						resultado <= n1 AND n2;
+						
+					else If esc = "0001" then 		--OR
+						resultado <= n1 OR n2;
+						
+					else If esc = "0010" then 		--NOT
+						resultado <= NOT n1;
+						
+						wait for 2s; 					--Aguarda 2 segundos para que apareça o próximo numero negado
+						
+						Resultado <= NOT n2;
+						
+					else If esc = "0011" then	 	--XOR
+						resultado <= n1 XOR n2;
+						
+				
+				-------------------------------------------Op.Artméticas
+					--else if esc = "0100" then 		--SOMA
+						--soma: somador_4bits PORT MAP (n1, n2, resultado,);	
+						
+					end if;					
+				end if;					
 	END PROCESS;
+	
 end Behavioral;
 
